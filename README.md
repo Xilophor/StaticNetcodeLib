6ba1cb5680f4e33b4b9f6c3ea363f86dc0170976
Xilophor <ryanzgreg@gmail.com>
2024-03-07 19:48:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# StaticNetcodeLib
+
+[![Build](https://img.shields.io/github/actions/workflow/status/Xilophor/StaticNetcodeLib/build.yml?style=for-the-badge&logo=github)](https://github.com/Xilophor/StaticNetcodeLib/actions/workflows/build.yml)
+[![Latest Version](https://img.shields.io/thunderstore/v/xilophor/StaticNetcodeLib?style=for-the-badge&logo=thunderstore&logoColor=white)](https://thunderstore.io/c/lethal-company/p/xilophor/StaticNetcodeLib)
+[![Thunderstore Downloads](https://img.shields.io/thunderstore/dt/xilophor/StaticNetcodeLib?style=for-the-badge&logo=thunderstore&logoColor=white)](https://thunderstore.io/c/lethal-company/p/xilophor/StaticNetcodeLib)
+[![NuGet Version](https://img.shields.io/nuget/v/Xilophor.StaticNetcodeLib?style=for-the-badge&logo=nuget)](https://www.nuget.org/packages/Xilophor.StaticNetcodeLib)
+
+This mod is an API meant for mod developers.
+
+## Usage
+
+
+
+## Acknowledgements
+
+Thank you [@Lordfirespeed](https://github.com/Lordfirespeed) for helping set up the GitHub Actions, and being my rubber ducky.
---
+++ StaticNetcodeLib/StaticNetcodeLib.cs
@@ -0,0 +1,43 @@
+﻿namespace StaticNetcodeLib;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class StaticNetcodeLib : BaseUnityPlugin
+{
+    public static StaticNetcodeLib Instance { get; private set; } = null!;
+    internal static new ManualLogSource Logger { get; private set; } = null!;
+    internal static Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        Patch();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    internal static void Patch()
+    {
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        Logger.LogDebug("Patching...");
+
+        Harmony.PatchAll();
+
+        Logger.LogDebug("Finished patching!");
+    }
+
+    internal static void Unpatch()
+    {
+        Logger.LogDebug("Unpatching...");
+
+        Harmony?.UnpatchSelf();
+
+        Logger.LogDebug("Finished unpatching!");
+    }
+}
