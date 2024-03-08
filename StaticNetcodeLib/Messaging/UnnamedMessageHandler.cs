77198f8fa9f3dea128c6ef69d8aeb2844949be7a
Xilophor <ryanzgreg@gmail.com>
2024-03-07 20:40:52 -0500

    Add Send Methods to Message Handler

--- StaticNetcodeLib/Messaging/UnnamedMessageHandler.cs
+++ StaticNetcodeLib/Messaging/UnnamedMessageHandler.cs
@@ -24,6 +24,45 @@ internal class UnnamedMessageHandler
         this.CustomMessagingManager = this.NetworkManager.CustomMessagingManager;
     }
 
+    #region Messaging
+
+    #region Send
+
+    internal void SendMessageToClient(MessageData messageData, ClientRpcParams clientRpcParams = default)
+    {
+        WriteMessageData(out var writer, messageData);
+
+        var clients = clientRpcParams.Send.TargetClientIds ??
+            clientRpcParams.Send.TargetClientIdsNativeArray.GetValueOrDefault().ToArray();
+
+        if (clients.Any())
+        {
+            this.CustomMessagingManager.SendUnnamedMessage(clients, writer,
+                NetworkDelivery.ReliableFragmentedSequenced);
+        }
+        else
+        {
+            this.CustomMessagingManager.SendUnnamedMessageToAll(writer,
+                NetworkDelivery.ReliableFragmentedSequenced);
+        }
+
+        writer.Dispose();
+    }
+
+    internal void SendMessageToServer(MessageData messageData)
+    {
+        WriteMessageData(out var writer, messageData);
+
+        this.CustomMessagingManager.SendUnnamedMessage(NetworkManager.ServerClientId, writer,
+            NetworkDelivery.ReliableFragmentedSequenced);
+
+        writer.Dispose();
+    }
+
+    #endregion
+
+    #endregion
+
     #region Helper Methods
 
     private static byte[] Serialize(object? data) => SerializationUtility.SerializeValue(data, DataFormat.Binary);
